d5a164fced8fbee4783d0d24963dd879d2d4b8aa
disdi <disdi@users.noreply.github.com>
2026-07-23 05:08:23 +0000

    deploy: 161d5c43c11c7254548d0099bbdacc873ecb46ae

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0f9a9adc.js',
+                path_to_root + 'searchindex-4d65906b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
